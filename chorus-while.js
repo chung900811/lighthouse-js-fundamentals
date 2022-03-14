c6d72bc78ce5a23f60969f73aa91b13b35419a40
lai chung ho <101287576+chung900811@users.noreply.github.com>
2022-03-13 23:15:57 -0400

    chorus-while.js
    git commit -m "Use a while loop to repeat a chorus line"

---
+++ chorus-while.js
@@ -0,0 +1,10 @@
+const chorus = "Let's dance!";
+let x = 0;
+while (x < 10) {
+  if (x === 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  x++;
+}
+console.log("Until the sun comes up!");
