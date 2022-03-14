a74c31e3378395e4b09083629eb8b5f7edc259d2
lai chung ho <101287576+chung900811@users.noreply.github.com>
2022-03-14 01:26:13 -0400

    even.js

---
+++ even.js
@@ -0,0 +1,9 @@
+const isEven  = function (num) {
+  return num % 5 === 0;
+}
+
+const twentyIsEven = isEven(20);
+const twentyOneIsEven = isEven(21);
+
+console.log(twenty);
+console.log(twentyOneIsEven);
